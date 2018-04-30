d8ef15d97087e65500d624d4a924c6009a642432
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-29 20:39:56 -0500

    control.

--- routes/control.js
+++ routes/control.js
@@ -1,6 +1,15 @@
 let express = require('express');
 let router = express.Router();
 
+let BITBOXCli = require('bitbox-cli/lib/bitboxcli').default;
+let BITBOX = new BITBOXCli({
+  protocol: 'http',
+  host: "138.68.54.100",
+  port: "8332",
+  username: "bitcoin",
+  password: "xhFjluMJMyOXcYvF"
+});
+
 router.get('/', function(req, res, next) {
   res.json({ status: 'control' });
 });
