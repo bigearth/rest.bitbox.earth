74e2792897269f9c35701bb637ab25540ffa3878
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-30 12:33:42 -0500

    files.

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(cors())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use("/public", express.static(__dirname + "/public"));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
