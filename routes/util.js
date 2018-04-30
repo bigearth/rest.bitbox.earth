bab3f06a25dbc62e2d2c54b1d67f3d21efab0bb9
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-30 09:54:43 -0500

    util

--- routes/util.js
+++ routes/util.js
@@ -1,40 +1,81 @@
 let express = require('express');
 let router = express.Router();
 
+let BITBOXCli = require('bitbox-cli/lib/bitboxcli').default;
+let BITBOX = new BITBOXCli({
+  protocol: 'http',
+  host: "138.68.54.100",
+  port: "8332",
+  username: "bitcoin",
+  password: "xhFjluMJMyOXcYvF"
+});
+
 router.get('/', function(req, res, next) {
   res.json({ status: 'util' });
 });
 
-router.get('/estimateSmartFee', function(req, res, next) {
-  res.json({ status: 'estimateSmartFee' });
+router.get('/estimateSmartFee/:nblocks', function(req, res, next) {
+  BITBOX.Util.estimateSmartFee(parseInt(req.params.nblocks))
+  .then((result) => {
+    res.json({ result: result });
+  }, (err) => { console.log(err);
+  });
 });
 
-router.get('/estimateSmartPriority', function(req, res, next) {
-  res.json({ status: 'estimateSmartPriority' });
+router.get('/estimateSmartPriority/:nblocks', function(req, res, next) {
+  BITBOX.Util.estimateSmartPriority(parseInt(req.params.nblocks))
+  .then((result) => {
+    res.json({ result: result });
+  }, (err) => { console.log(err);
+  });
 });
 
-router.get('/createMultisig', function(req, res, next) {
-  res.json({ status: 'createMultisig' });
+router.get('/createMultisig/:nrequired/:keys', function(req, res, next) {
+  BITBOX.Util.createMultisig(parseInt(req.params.nrequired), req.params.keys)
+  .then((result) => {
+    res.json({ result: result });
+  }, (err) => { console.log(err);
+  });
 });
 
-router.get('/estimateFee', function(req, res, next) {
-  res.json({ status: 'estimateFee' });
+router.get('/estimateFee/:nblocks', function(req, res, next) {
+  BITBOX.Util.estimateFee(parseInt(req.params.nblocks))
+  .then((result) => {
+    res.json({ result: result });
+  }, (err) => { console.log(err);
+  });
 });
 
-router.get('/estimatePriority', function(req, res, next) {
-  res.json({ status: 'estimatePriority' });
+router.get('/estimatepriority/:nblocks', function(req, res, next) {
+  BITBOX.Util.estimatepriority(parseInt(req.params.nblocks))
+  .then((result) => {
+    res.json({ result: result });
+  }, (err) => { console.log(err);
+  });
 });
 
-router.get('/signMessageWithPrivKey', function(req, res, next) {
-  res.json({ status: 'signMessageWithPrivKey' });
+router.get('/signMessageWithPrivKey/:privkey/:message', function(req, res, next) {
+  BITBOX.Util.signMessageWithPrivKey(req.params.privkey, req.params.message)
+  .then((result) => {
+    res.json({ result: result });
+  }, (err) => { console.log(err);
+  });
 });
 
-router.get('/validateAddress', function(req, res, next) {
-  res.json({ status: 'validateAddress' });
+router.get('/validateAddress/:address', function(req, res, next) {
+  BITBOX.Util.validateAddress(req.params.address)
+  .then((result) => {
+    res.json({ result: result });
+  }, (err) => { console.log(err);
+  });
 });
 
-router.get('/verifyMessage', function(req, res, next) {
-  res.json({ status: 'verifyMessage' });
+router.get('/verifyMessage/:address/:signature/:message', function(req, res, next) {
+  BITBOX.Util.verifyMessage(req.params.address, req.params.signature, req.params.message)
+  .then((result) => {
+    res.json({ result: result });
+  }, (err) => { console.log(err);
+  });
 });
 
 module.exports = router;
