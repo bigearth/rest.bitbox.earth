09cb0bd53c3fbc317b866589c1f24bfc2c33f884
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-29 19:35:01 -0500

    util.

--- routes/util.js
+++ routes/util.js
@@ -1,9 +1,40 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ status: 'util' });
+});
+
+router.get('/estimateSmartFee', function(req, res, next) {
+  res.json({ status: 'estimateSmartFee' });
+});
+
+router.get('/estimateSmartPriority', function(req, res, next) {
+  res.json({ status: 'estimateSmartPriority' });
+});
+
+router.get('/createMultisig', function(req, res, next) {
+  res.json({ status: 'createMultisig' });
+});
+
+router.get('/estimateFee', function(req, res, next) {
+  res.json({ status: 'estimateFee' });
+});
+
+router.get('/estimatePriority', function(req, res, next) {
+  res.json({ status: 'estimatePriority' });
+});
+
+router.get('/signMessageWithPrivKey', function(req, res, next) {
+  res.json({ status: 'signMessageWithPrivKey' });
+});
+
+router.get('/validateAddress', function(req, res, next) {
+  res.json({ status: 'validateAddress' });
+});
+
+router.get('/verifyMessage', function(req, res, next) {
+  res.json({ status: 'verifyMessage' });
 });
 
 module.exports = router;
