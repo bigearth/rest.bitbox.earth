95216a6704590d3a25517fddb0e4f9fa0c32afe7
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-29 19:47:59 -0500

    mining

--- routes/mining.js
+++ routes/mining.js
@@ -1,9 +1,28 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ status: 'mining' });
+});
+
+router.get('/getBlockTemplate', function(req, res, next) {
+  res.json({ status: 'getBlockTemplate' });
+});
+
+router.get('/getMiningInfo', function(req, res, next) {
+  res.json({ status: 'getMiningInfo' });
+});
+
+router.get('/getNetworkHashps', function(req, res, next) {
+  res.json({ status: 'getNetworkHashps' });
+});
+
+router.get('/prioritiseTransaction', function(req, res, next) {
+  res.json({ status: 'prioritiseTransaction' });
+});
+
+router.get('/submitBlock', function(req, res, next) {
+  res.json({ status: 'submitBlock' });
 });
 
 module.exports = router;
