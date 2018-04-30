6e70cbe88a66be8148a7b29703c884e4403d0b7a
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-29 19:44:01 -0500

    network.

--- routes/network.js
+++ routes/network.js
@@ -1,9 +1,52 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ status: 'network' });
+});
+
+router.get('/addNode', function(req, res, next) {
+  res.json({ status: 'addNode' });
+});
+
+router.get('/clearBanned', function(req, res, next) {
+  res.json({ status: 'clearBanned' });
+});
+
+router.get('/disconnectNode', function(req, res, next) {
+  res.json({ status: 'disconnectNode' });
+});
+
+router.get('/getAddedNodeInfo', function(req, res, next) {
+  res.json({ status: 'getAddedNodeInfo' });
+});
+
+router.get('/getConnectionCount', function(req, res, next) {
+  res.json({ status: 'getConnectionCount' });
+});
+
+router.get('/getNetTotals', function(req, res, next) {
+  res.json({ status: 'getNetTotals' });
+});
+
+router.get('/getNetworkInfo', function(req, res, next) {
+  res.json({ status: 'getNetworkInfo' });
+});
+
+router.get('/getPeerInfo', function(req, res, next) {
+  res.json({ status: 'getPeerInfo' });
+});
+
+router.get('/ping', function(req, res, next) {
+  res.json({ status: 'ping' });
+});
+
+router.get('/setBan', function(req, res, next) {
+  res.json({ status: 'setBan' });
+});
+
+router.get('/setNetworkActive', function(req, res, next) {
+  res.json({ status: 'setNetworkActive' });
 });
 
 module.exports = router;
