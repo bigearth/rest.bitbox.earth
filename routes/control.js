7f5ffe5ebedd0c7779dcaaa77ab12d5d298e0579
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-29 19:52:32 -0500

    Control.

--- routes/control.js
+++ routes/control.js
@@ -1,9 +1,24 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ status: 'control' });
+});
+
+router.get('/getInfo', function(req, res, next) {
+  res.json({ status: 'getInfo' });
+});
+
+router.get('/getMemoryInfo', function(req, res, next) {
+  res.json({ status: 'getMemoryInfo' });
+});
+
+router.get('/help', function(req, res, next) {
+  res.json({ status: 'help' });
+});
+
+router.get('/stop', function(req, res, next) {
+  res.json({ status: 'stop' });
 });
 
 module.exports = router;
