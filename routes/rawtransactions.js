1d54682d91251397b8e9d58c9e252051b6e676c6
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-29 19:39:21 -0500

    rt

--- routes/rawtransactions.js
+++ routes/rawtransactions.js
@@ -1,9 +1,24 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ status: 'rawtransaction' });
+});
+
+router.get('/decodeRawTransaction', function(req, res, next) {
+  res.json({ status: 'decodeRawTransaction' });
+});
+
+router.get('/decodeScript', function(req, res, next) {
+  res.json({ status: 'decodeScript' });
+});
+
+router.get('/getRawTransaction', function(req, res, next) {
+  res.json({ status: 'getRawTransaction' });
+});
+
+router.get('/sendRawTransaction', function(req, res, next) {
+  res.json({ status: 'sendRawTransaction' });
 });
 
 module.exports = router;
