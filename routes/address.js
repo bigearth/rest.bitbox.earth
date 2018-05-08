46fcaf95a330c4f0fe511eac6af3cd0e6831b02f
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-05-07 20:25:45 -0700

    utxo multiple.

--- routes/address.js
+++ routes/address.js
@@ -42,17 +42,31 @@ router.get('/details/:address', function(req, res, next) {
 });
 
 router.get('/utxo/:address', function(req, res, next) {
-  axios.get(`https://explorer.bitcoin.com/api/bch/addr/${BITBOX.Address.toLegacyAddress(req.params.address)}/utxo`)
-  .then((result) => {
-    result.data.forEach((data) => {
-      delete data.address;
-      data.legacyAddress = BITBOX.Address.toLegacyAddress(req.params.address);
-      data.cashAddress = BITBOX.Address.toCashAddress(req.params.address);
+  try {
+    let addresses = JSON.parse(req.params.address);
+    let result = [];
+    addresses = addresses.map(function(address) {
+      return BITBOX.Address.utxo(address)
     })
+    axios.all(addresses)
+    .then(axios.spread(function (...spread) {
+      result.push(...spread);
+      res.json(result);
+    }));
+  }
+  catch(error) {
+    axios.get(`https://explorer.bitcoin.com/api/bch/addr/${BITBOX.Address.toLegacyAddress(req.params.address)}/utxo`)
+    .then((result) => {
+      result.data.forEach((data) => {
+        delete data.address;
+        data.legacyAddress = BITBOX.Address.toLegacyAddress(req.params.address);
+        data.cashAddress = BITBOX.Address.toCashAddress(req.params.address);
+      })
 
-    res.json(result.data);
-  }, (err) => { console.log(err);
-  });
+      res.json(result.data);
+    }, (err) => { console.log(err);
+    });
+  }
 });
 
 module.exports = router;
