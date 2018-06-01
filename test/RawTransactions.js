c44b44dea04ed80ffe126f8c939971ccc759b775
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-05-31 18:20:12 -0700

    RawTransactions

--- test/RawTransactions.js
+++ test/RawTransactions.js
@@ -57,4 +57,42 @@ describe("#RawTransactionsRouter", () => {
       });
     });
   });
+
+  describe("#GetRawTransaction", () => {
+    it("should GET /getRawTransaction/:txid", (done) => {
+      let mockRequest = httpMocks.createRequest({
+        method: "GET",
+          url: "/getRawTransaction/0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098"
+      });
+      let mockResponse = httpMocks.createResponse({
+        eventEmitter: require('events').EventEmitter
+      });
+      rawTransactionsRoute(mockRequest, mockResponse);
+
+      mockResponse.on('end', () => {
+        let actualResponseBody = JSON.parse(mockResponse._getData());
+        assert.equal(actualResponseBody, '01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0704ffff001d0104ffffffff0100f2052a0100000043410496b538e853519c726a2c91e61ec11600ae1390813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e62294721166bf621e73a82cbf2342c858eeac00000000');
+        done();
+      });
+    });
+  });
+
+  describe("#SendRawTransaction", () => {
+    it("should POST /sendRawTransaction/:hex", (done) => {
+      let mockRequest = httpMocks.createRequest({
+        method: "POST",
+          url: "/sendRawTransaction/01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0704ffff001d0104ffffffff0100f2052a0100000043410496b538e853519c726a2c91e61ec11600ae1390813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e62294721166bf621e73a82cbf2342c858eeac00000000"
+      });
+      let mockResponse = httpMocks.createResponse({
+        eventEmitter: require('events').EventEmitter
+      });
+      rawTransactionsRoute(mockRequest, mockResponse);
+
+      mockResponse.on('end', () => {
+        let actualResponseBody = JSON.parse(mockResponse._getData());
+        assert.equal(actualResponseBody, '"transaction already in block chain"');
+        done();
+      });
+    });
+  });
 });
