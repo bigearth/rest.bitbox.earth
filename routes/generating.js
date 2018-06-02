b0f405d4b900579c4058af68f39d1285f4704f40
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-06-02 16:01:23 -0700

    generating.

--- routes/generating.js
+++ routes/generating.js
@@ -1,8 +1,15 @@
 let express = require('express');
 let router = express.Router();
+let axios = require('axios');
 
 let BITBOXCli = require('bitbox-cli/lib/bitbox-cli').default;
 let BITBOX = new BITBOXCli();
+
+let BitboxHTTP = axios.create({
+  baseURL: `http://138.68.54.100:8332/`
+});
+let username = 'bitcoin';
+let password = 'xhFjluMJMyOXcYvF';
 
 router.get('/', (req, res, next) => {
   res.json({ status: 'generating' });
@@ -13,10 +20,29 @@ router.post('/generateToAddress/:nblocks/:address', (req, res, next) => {
   if(req.query.maxtries) {
     maxtries = parseInt(req.query.maxtries);
   }
-  BITBOX.Generating.generateToAddress(req.params.nblocks, req.params.address, maxtries)
-  .then((result) => {
-    res.json(result);
-  }, (err) => { console.log(err);
+
+  BitboxHTTP({
+    method: 'post',
+    auth: {
+      username: username,
+      password: password
+    },
+    data: {
+      jsonrpc: "1.0",
+      id:"generatetoaddress",
+      method: "generatetoaddress",
+      params: [
+        req.params.nblocks,
+        req.params.address,
+        maxtries
+      ]
+    }
+  })
+  .then((response) => {
+    res.send(response.data.result);
+  })
+  .catch((error) => {
+    res.send(error.response.data.error.message);
   });
 });
 
--- test/generating.js
+++ test/generating.js
@@ -5,39 +5,38 @@ let httpMocks = require("node-mocks-http");
 let generatingRoute = require('../routes/generating');
 
 describe("#GeneratingRouter", () => {
-    describe("#root", () => {
-      it("should return 'generating' for GET /", () => {
-        let mockRequest = httpMocks.createRequest({
-          method: "GET",
-          url: "/"
-        });
-        let mockResponse = httpMocks.createResponse();
-        generatingRoute(mockRequest, mockResponse);
-        let actualResponseBody = mockResponse._getData();
-        let expectedResponseBody = {
-          status: 'generating'
-        };
-        assert.deepEqual(JSON.parse(actualResponseBody), expectedResponseBody);
+  describe("#root", () => {
+    it("should return 'generating' for GET /", () => {
+      let mockRequest = httpMocks.createRequest({
+        method: "GET",
+        url: "/"
       });
+      let mockResponse = httpMocks.createResponse();
+      generatingRoute(mockRequest, mockResponse);
+      let actualResponseBody = mockResponse._getData();
+      let expectedResponseBody = {
+        status: 'generating'
+      };
+      assert.deepEqual(JSON.parse(actualResponseBody), expectedResponseBody);
     });
+  });
 
-    describe("#GeneratingGenerateToAddress", () => {
-      it("should POST /generateToAddress/:n/:address ", (done) => {
-        let mockRequest = httpMocks.createRequest({
-          method: "POST",
-          url: "/generateToAddress/1/qrff52mj0ml4scljzxrex7ses2gst42k9sfz2lftjq"
-        });
-        let mockResponse = httpMocks.createResponse({
-          eventEmitter: require('events').EventEmitter
-        });
-        generatingRoute(mockRequest, mockResponse);
+  describe("#GeneratingGenerateToAddress", () => {
+    it("should POST /generateToAddress/:n/:address ", (done) => {
+      let mockRequest = httpMocks.createRequest({
+        method: "POST",
+        url: "/generateToAddress/1/qrff52mj0ml4scljzxrex7ses2gst42k9sfz2lftjq"
+      });
+      let mockResponse = httpMocks.createResponse({
+        eventEmitter: require('events').EventEmitter
+      });
+      generatingRoute(mockRequest, mockResponse);
 
-        mockResponse.on('end', () => {
-          let actualResponseBody = JSON.parse(mockResponse._getData());
-          // TODO: not just make this equal to a string..
-          assert.equal(actualResponseBody, "\"JSON value is not an integer as expected\"");
-          done();
-        });
+      mockResponse.on('end', () => {
+        let actualResponseBody = mockResponse._getData();
+        assert.equal(actualResponseBody, "JSON value is not an integer as expected");
+        done();
       });
     });
   });
+});
