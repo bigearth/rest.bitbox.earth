1015b60663a02bd40d46807d9a25613d8690d533
Gabriel Cardona <gabriel@bitcoin.com>
2018-06-27 11:58:47 +0900

    Mainnet creds.

--- routes/blockchain.js
+++ routes/blockchain.js
@@ -6,18 +6,18 @@ let BITBOXCli = require('bitbox-cli/lib/bitbox-cli').default;
 let BITBOX = new BITBOXCli();
 
 //testnet creds
-let BitboxHTTP = axios.create({
-  baseURL: `http://167.99.110.201:18332/`
-});
-let username = 'bitcoin';
-let password = 'rD9yw2Y9RkOnYjYnDX';
-
-// mainnet creds
 // let BitboxHTTP = axios.create({
-//   baseURL: `http://138.68.54.100:8332/`
+//   baseURL: `http://167.99.110.201:18332/`
 // });
 // let username = 'bitcoin';
-// let password = 'xhFjluMJMyOXcYvF';
+// let password = 'rD9yw2Y9RkOnYjYnDX';
+
+// mainnet creds
+let BitboxHTTP = axios.create({
+  baseURL: `http://138.68.54.100:8332/`
+});
+let username = 'bitcoin';
+let password = 'xhFjluMJMyOXcYvF';
 
 router.get('/', (req, res, next) => {
   res.json({ status: 'blockchain' });
