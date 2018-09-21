492ff756b4a3a4684e711eea75237110dc183b1a
Chris Troutner <trout@pop-os.localdomain>
2018-09-21 12:54:34 -0700

    minor change to catch block

--- routes/rawtransactions.js
+++ routes/rawtransactions.js
@@ -404,7 +404,7 @@ router.post(
         res.status(500).send(error.response.data.error)
       }
     } catch (err) {
-      console.log(`Error in /change: `, err)
+      console.log(`Error in /change: `)
       res.status(500)
       res.send(`Error in /change: ${err.message}`)
     }
